2df738c7bbe879990dc0bfed8f11d8bfd82bb6a4
FilipNastovski <148493743+FilipNastovski@users.noreply.github.com>
2024-11-12 22:08:23 +0100

    Update symbol extraction

--- IssuerCodeExtractor.py
+++ IssuerCodeExtractor.py
@@ -53,13 +53,8 @@ class IssuerCodeExtractor:
         return unique_codes
 
     def filter_codes(self, codes: List[str]) -> List[str]:
-        useless_issuer_codes = (
-        'OPFO', 'PROT', 'VFPM', 'TTKO', 'OMOS', 'FKTL', 'INOV', 'CKBKO', 'KKST', 'GRDN', 'TSZS', 'JAKO', 'KULT', 'VSC',
-        'LHND', 'LAJO', 'PTRS', 'OSPO', 'GDKM', 'VARG', 'CBNG', 'OBPP', 'FROT', 'JUSK', 'KORZ', 'TRUB', 'PELK', 'RINS',
-        'TBKO', 'IJUG', 'KMPR', 'GRSN', 'ZUAS', 'PGGV', 'CDHV', 'BIKF', 'KDFO', 'SNBTO', 'SNBT', 'ENSA', 'MAGP', 'MLKR',
-        'ELNC', 'MPTE', 'EDST', 'SKON', 'EUMK', 'ENER', 'ELMA')
         return [code for code in codes if
-                not any(char.isdigit() for char in code)  and code not in useless_issuer_codes]
+                not any(char.isdigit() for char in code)]
 
     def get_data(self) -> List[str]:
         return self.get_issuer_codes()
